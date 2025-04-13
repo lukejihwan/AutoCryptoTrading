a2bd5af174f40c8bc3a263f64ec01686f7f2491e
lukejihwan <luke9701@naver.com>
2025-04-13 23:55:31 +0900

    update

--- getPreviousPrice.py
+++ getPreviousPrice.py
@@ -5,3 +5,11 @@ print(btc)
 
 close = btc['close']
 print(close)
+
+print((close[0] + close[1] + close[2] + close[3] + close[4] / 5))
+print((close[1] + close[2] + close[3] + close[4] + close[5] / 5))
+print((close[2] + close[3] + close[4] + close[5] + close[6] / 5))
+
+window = close.rolling(5)
+ma5 = window.mean()
+print(ma5)
