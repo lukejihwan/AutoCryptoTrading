c4132ae5038d254e56ada741034e97f0040235c4
lukejihwan <luke9701@naver.com>
2025-04-16 23:39:26 +0900

    api_key를 통해 잔고 호출

---
+++ privateKeyTest.py
@@ -0,0 +1,13 @@
+import pybithumb
+import time
+
+# Set API parameters
+con_key = ''
+sec_key = ''
+
+bithumb = pybithumb.Bithumb(con_key, sec_key)
+
+for ticker in pybithumb.get_tickers():
+    balance = bithumb.get_balance(ticker)
+    print(ticker, ":", balance)
+    time.sleep(0.1)
