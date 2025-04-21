0d762e7199874ddbf3527a98c7d9a7b6c8193472
jihwanhwang1 <jihwan.hwang@rethinkmall.com>
2025-04-21 23:59:28 +0900

    update

--- privateKeyTest.py
+++ privateKeyTest.py
@@ -13,4 +13,9 @@ for ticker in pybithumb.get_tickers():
     time.sleep(0.1)
     print(format(balance[0], 'f'))
 
+for ticker in pybithumb.get_tickers():
+    balance = bithumb.get_balance(ticker)
+    print(ticker, ":", balance)
+    time.sleep(0.1)
+    print(format(balance[0], 'f'))
 
