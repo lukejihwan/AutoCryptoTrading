9c6e86ead7420b0e8fb1b73693d2d7b4d79ea9b8
jihwanhwang1 <jihwan.hwang@rethinkmall.com>
2025-04-23 23:57:01 +0900

    지정가 매수

--- privateKeyTest.py
+++ privateKeyTest.py
@@ -13,9 +13,7 @@ for ticker in pybithumb.get_tickers():
     time.sleep(0.1)
     print(format(balance[0], 'f'))
 
-for ticker in pybithumb.get_tickers():
-    balance = bithumb.get_balance(ticker)
-    print(ticker, ":", balance)
-    time.sleep(0.1)
-    print(format(balance[0], 'f'))
+# 지정가 매수하는 법
+order = bithumb.buy_limit_order('BTC', 130000000, 0.001)
+print(order)
 
