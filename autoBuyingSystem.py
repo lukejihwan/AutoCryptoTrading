f323f89bf5d39a60fe0e11d76cd53038a4adc511
jihwanhwang1 <jihwan.hwang@rethinkmall.com>
2025-05-06 00:18:15 +0900

    backTesting진행

--- autoBuyingSystem.py
+++ autoBuyingSystem.py
@@ -17,9 +17,19 @@ def get_target_price(ticker):
     target = today_open + (yesterday_high - yesterday_low) * 0.5
     return target
 
+def get_yesterday_ma5(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    close = df['close']
+    ma = close.rolling(window=5).mean()
+    return ma[-2]
+
 now = datetime.datetime.now()
 mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+# 상승장인지 체크
+ma5 = get_yesterday_ma5('BTC')
 target_price = get_target_price('BTC')
+
 # private API 설정
 con_key = os.getenv('con_key')
 sec_key = os.getenv('secret_key')
@@ -27,17 +37,22 @@ sec_key = os.getenv('secret_key')
 bithumb = pybithumb.Bithumb(con_key, sec_key)
 
 while True:
-    now = datetime.datetime.now()
-    if mid < now < mid + datetime.timedelta(seconds=10):
-        target_price = get_target_price('BTC')
-        mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+    try:
+        now = datetime.datetime.now()
+        if mid < now < mid + datetime.timedelta(seconds=10):
+            target_price = get_target_price('BTC')
+            mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+            ma5 = get_yesterday_ma5('BTC')
 
-    current_price = pybithumb.get_current_price('BTC')
+        current_price = pybithumb.get_current_price('BTC')
 
-    if current_price > target_price:
-        krw = bithumb.get_balance('BTC')[2]
-        orderbook = pybithumb.get_orderbook('BTC')
-        sell_price = orderbook['asks'][0]['price']
-        unit = krw/float(sell_price)
-        bithumb.buy_market_order('BTC', unit)
+        if current_price > target_price:
+            krw = bithumb.get_balance('BTC')[2]
+            orderbook = pybithumb.get_orderbook('BTC')
+            sell_price = orderbook['asks'][0]['price']
+            unit = krw/float(sell_price)
+            bithumb.buy_market_order('BTC', unit)
+    except:
+        print('에러발생')
+    time.sleep(1)
 
