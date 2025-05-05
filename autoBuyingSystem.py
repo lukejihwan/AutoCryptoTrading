e8495fce004354937728f8bab56c5abe913df73f
jihwanhwang1 <jihwan.hwang@rethinkmall.com>
2025-05-05 16:57:42 +0900

    env파일 생성 자동매매 개발

---
+++ autoBuyingSystem.py
@@ -0,0 +1,43 @@
+import os
+import time
+import pybithumb
+import datetime
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_target_price(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    yesterday = df.iloc[-2]
+
+    today_open = yesterday['close']
+    yesterday_high = yesterday['high']
+    yesterday_low = yesterday['low']
+    target = today_open + (yesterday_high - yesterday_low) * 0.5
+    return target
+
+now = datetime.datetime.now()
+mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+target_price = get_target_price('BTC')
+# private API 설정
+con_key = os.getenv('con_key')
+sec_key = os.getenv('secret_key')
+
+bithumb = pybithumb.Bithumb(con_key, sec_key)
+
+while True:
+    now = datetime.datetime.now()
+    if mid < now < mid + datetime.timedelta(seconds=10):
+        target_price = get_target_price('BTC')
+        mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+    current_price = pybithumb.get_current_price('BTC')
+
+    if current_price > target_price:
+        krw = bithumb.get_balance('BTC')[2]
+        orderbook = pybithumb.get_orderbook('BTC')
+        sell_price = orderbook['asks'][0]['price']
+        unit = krw/float(sell_price)
+        bithumb.buy_market_order('BTC', unit)
+
--- volatilityTest.py
+++ volatilityTest.py
@@ -3,5 +3,7 @@ import time
 
 while True:
     price = pybithumb.get_current_price('BTC')
+    df = pybithumb.get_ohlcv('BTC')
     print(price)
+    print(df.tail())
     time.sleep(0.2)
